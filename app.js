4c44f9bbaed38d8276cd58a0f713f08e68c53132
FerOrtiz97 <tino.pipe97@gmail.com>
2023-10-20 05:00:13 -0300

    modifique algunos archivos

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 5000
+const port = 5000;
 
-app.listen(port,() => {
-    console.log(`Servidor levantado en https://localhost:${port}.com`)});
-    
+app.listen(port, () => {
+    console.log(`Servidor levantado en https://localhost:${port}.com`);
+});
+
 app.get("/", (req, res) => {
-    res.sendfile(path.join(__dirname,`/views/por.html`))});
+    res.sendfile(path.join(__dirname, `/views/home.html`));
+});
 
-
-    app.use(express.static(__dirname + '/public'));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // app.get("/", (req, res) => {
-    //     res.sendFile(path.join(__dirname, '/views/home.html'))
-    // })
+app.use(express.static(__dirname + "/public"));
